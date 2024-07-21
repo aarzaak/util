a63f69b68efbc47cf7ff3ecb55f0ab94f6f29c6c
aarzaak <139879547+aarzaak@users.noreply.github.com>
2024-07-21 19:24:26 +0200

    init.sh now uses functions from util.sh

--- rpi0/init.sh
+++ rpi0/init.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 
-echo -e "### Time to remove unused kernels: ###"
-sudo apt remove linux-*v7 linux-*v7l linux-*v8 -y
-echo -e "### Kernels were removed. ###\n"
+. ./ressources/libraries/util.sh
 
-echo -e "### Time to upgrade: ###"
-sudo apt update
-sudo apt full-upgrade -y
-echo -e "### System was upgraded. ###\n"
-
-echo -e "### Time to install basic tools (needrestart): ###"
-sudo apt install needrestart -y
-echo -e "### Basic tools were installed. ###\n"
-
-echo -e "### Time to fix that nasty warning we get when updating repositories: ###"
-echo -e "### Exporting key stored in trusted.gpg and storing it the right way… ###"
-sudo apt-key export 90FDDD2E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/raspberrypi-debian-armhf.gpg
-echo -e "### Backing up files… ###"
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
-echo -e "### Files were backed up. ###\n"
-echo -e "### Upgrading .list files… ###\n"
-sudo cp files/sources.list /etc/apt/
-sudo cp files/raspi.list /etc/apt/sources.list.d/
-echo -e "### .list files were upgraded. ###\n"
-
-echo -e "### Should you reboot? ###"
-sudo needrestart
-
-yesnoanswer="null"
-while [[ ! ${yesnoanswer,,} =~ ^([yn]|yes|no)?$ ]]; do
-	read -r -p "Would you like to reboot? [y/n] " yesnoanswer
-done
-case ${yesnoanswer,,} in
-	y|yes) sudo shutdown -r now;;
-	n|no) exit 0;;
-	*) echo "An error has occurred, this is unfortunate… ¯\_(ツ)_/¯";;
-esac
-
+removeUnusedKernels
+upgradeSystem
+installBasicTools
+fixAptWarning
+checkRebootRequired
+askReboot
