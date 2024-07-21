2ad0f7f6386e26d617a2abfbb8b2d62204ce11bb
aarzaak <139879547+aarzaak@users.noreply.github.com>
2024-07-21 20:06:13 +0200

    console texte style is now properly reset

--- rpi0/ressources/libraries/util.sh
+++ rpi0/ressources/libraries/util.sh
@@ -7,57 +7,57 @@ FILES_DIR="./ressources/files"
 askReboot() {
 	yesnoanswer="null"
 	while [[ ! ${yesnoanswer,,} =~ ^([yn]|yes|no)?$ ]]; do
-		read -r -p "${BLUE}Would you like to reboot? [y/n] " yesnoanswer
+		read -r -p "${BLUE}Would you like to reboot? [y/n]${NS} " yesnoanswer
 	done
 	case ${yesnoanswer,,} in
-		y|yes) printf "${GREEN}Rebooting…"; sleep 3; sudo shutdown -r now;;
+		y|yes) printf "${GREEN}Rebooting…${NS}"; sleep 3; sudo shutdown -r now;;
 		n|no) exit 0;;
 		*) printf "${RED}An error has occurred.${NS} Well, this is awkward… Would you please file a defect report?\n";;
 	esac
 }
 
 checkRebootRequired() {
-	printf "${BLUE}Should you reboot?\n"
+	printf "${BLUE}Should you reboot?${NS}\n"
 	sudo needrestart
 }
 
 removeUnusedKernels() {
-	printf "${BLUE}Removing unused kernels…\n"
+	printf "${BLUE}Removing unused kernels…${NS}\n"
 	sudo apt remove linux-*v7 linux-*v7l linux-*v8 -y
-	printf "${GREEN}Unused kernels were removed.\n"
+	printf "${GREEN}Unused kernels were removed.${NS}\n"
 }
 
 upgradeSystem() {
-	printf "${BLUE}Looking for system updates…\n"
+	printf "${BLUE}Looking for system updates…${NS}\n"
 	sudo apt update
-	printf "${GREEN}Repositories were updated.\n"
+	printf "${GREEN}Repositories were updated.${NS}\n"
 	
-	printf "${BLUE}Upgrading system…\n"
+	printf "${BLUE}Upgrading system…${NS}\n"
 	sudo apt full-upgrade -y
-	printf "${GREEN}System is now up-to-date.\n"
+	printf "${GREEN}System is now up-to-date.${NS}\n"
 }
 
 installBasicTools() {
-	printf "${BLUE}Installing basic tools (needrestart)…\n"
+	printf "${BLUE}Installing basic tools (needrestart)…${NS}\n"
 	sudo apt install needrestart -y
-	printf "${GREEN}Basic tools were installed.\n"
+	printf "${GREEN}Basic tools were installed.${NS}\n"
 }
 
 fixAptWarning() {
-	printf "${BLUE}Fixing apt warning when updating repositories…\n"
+	printf "${BLUE}Fixing apt warning when updating repositories…${NS}\n"
 
-	printf "${BLUE}Exporting key stored in trusted.gpg and storing it the right way…\n"
+	printf "${BLUE}Exporting key stored in trusted.gpg and storing it the right way…${NS}\n"
 	sudo apt-key export 90FDDD2E | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/raspberrypi-debian-armhf.gpg
-	printf "${GREEN}Key was exported and stored.\n"
+	printf "${GREEN}Key was exported and stored.${NS}\n"
 		
-	printf "${BLUE}Backing up .list files…\n"
+	printf "${BLUE}Backing up .list files…${NS}\n"
 	sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 	sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
-	printf "${GREEN}.list files were backed up.\n"
+	printf "${GREEN}.list files were backed up.${NS}\n"
 
-	printf "${BLUE}Upgrading .list files…\n"
+	printf "${BLUE}Upgrading .list files…${NS}\n"
 	sudo cp $FILES_DIR/sources.list /etc/apt
 	sudo cp $FILES_DIR/raspi.list /etc/apt/sources.list.d
-	printf "${GREEN}.list files were upgraded and apt warning is now gone.\n"
+	printf "${GREEN}.list files were upgraded and apt warning is now gone.${NS}\n"
 	
 }
