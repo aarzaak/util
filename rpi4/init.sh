eac743fbbcb526792682fc5dc6f0922400c323dd
aarzaak <139879547+aarzaak@users.noreply.github.com>
2025-08-16 17:52:42 +0200

    fixed a typo

--- rpi4/init.sh
+++ rpi4/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt remove linux-*v6 linux-v7* inux-*2712 -y
+sudo apt remove linux-*v6 linux-v7* linux-*2712 -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean
